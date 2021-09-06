d57225bdfa0242c08497cb58a2213c2fcabd1a9a
maguredhi <genso1104@gmail.com>
2021-09-06 11:39:58 +0800

    add flash notice

--- app/javascript/styles/shared/index.js
+++ app/javascript/styles/shared/index.js
@@ -22,3 +22,14 @@ document.addEventListener('turbolinks:load', () => {
   }
 
 });
+
+// delete button
+document.addEventListener('turbolinks:load', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
