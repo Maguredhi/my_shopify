8968ddcac9e52bc30b5ee39aac2b6ed752ba8594
maguredhi <genso1104@gmail.com>
2021-09-07 15:58:27 +0800

    add product CRUD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
+  include CodeGenerator
+  acts_as_paranoid
+
+  validates :name, presence: true
+  validates :list_price, :sell_price, numericality: { greater_than: 0, allow_nil: true }
+  validates :code, uniqueness: true
+
   belongs_to :vendor
 end
