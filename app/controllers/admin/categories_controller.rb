fb7c5fcfc3badd220ecc724ece4188febf6d1c1e
maguredhi <genso1104@gmail.com>
2021-09-10 10:03:48 +0800

    1. add drag and drop gem 2. add category CRUD

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,47 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
+    @categories = Category.all.order(position: :asc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_path, notice: '新增分類成功'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to edit_admin_category_path(@category), notice: '編輯分類成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin_categories_path, notice: '編輯刪除成功'
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
