f65d30850db8a2bca3bac7cfa24866361c8566a5
maguredhi <genso1104@gmail.com>
2021-09-10 17:39:24 +0800

    1. add rexml gem to fix rspec errors 2. remove test folder

--- spec/factories/skus.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :sku do
-    product { nil }
-    spec { "MyString" }
-    quantity { 1 }
-    deleted_at { "2021-09-07 16:25:15" }
-  end
-end
--- spec/factories/vendors.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :vendor do
-    
-  end
-end
